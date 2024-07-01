68653d35cbbe1b96924452af2163527cbe4250c1
Surya Prakash Mallah <sfrost488@gmail.com>
2024-07-01 19:02:23 +0530

    initial commit

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import connectDB from './config/db.js'
+const app = express()
+const port = 3000
+
+connectDB()
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
