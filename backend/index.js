fb0add64c605ad924f329c8aa545b31bbea9139d
Surya Prakash Mallah <sfrost488@gmail.com>
2024-07-02 18:48:17 +0530

    added cors to the backend server

--- backend/index.js
+++ backend/index.js
@@ -4,11 +4,13 @@ import dotenv from 'dotenv'
 dotenv.config()
 import connectDB from './config/db.js'
 import userRouter from './routes/user.route.js'
+import cors from 'cors'
 const app = express()
 const port = process.env.PORT || 3000
 
 connectDB()
 
+app.use(cors())
 //middleware
 app.use(express.json())
 
